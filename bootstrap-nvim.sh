e896e8f987a22eccffb7d7a791e6b1e4a6834fae
Harm Brouwer <me@hbrouwer.eu>
2022-04-08 14:32:48 +0200

    vim: call bootstrap-vim.sh from bootstrap-nvim.sh

--- bootstrap-nvim.sh
+++ bootstrap-nvim.sh
@@ -1,9 +1,3 @@
 mkdir -p ~/.config/nvim
 cp .config/nvim/init.vim ~/.config/nvim/init.vim
-mkdir -p ~/.vim
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.vim/.undo
-mkdir -p ~/.vim/.backup
-mkdir -p ~/.vim/.swp
-cp .vimrc ~/.vimrc
+sh bootstrap-vim.sh
---
+++ bootstrap-vim.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.vim
+mkdir -p ~/.vim/autoload
+curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/.undo
+mkdir -p ~/.vim/.backup
+mkdir -p ~/.vim/.swp
+cp .vimrc ~/.vimrc
